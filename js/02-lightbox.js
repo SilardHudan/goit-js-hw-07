98016c4bb5367fbc81a613321b3fd1e04bf089f2
Silard Hudan <silard.gudan@gmail.com>
2022-08-14 01:05:51 +0300

    14.08.22

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const items = []
+
+galleryItems.forEach(element => {
+	const galleryItems = document.createElement('li')
+	galleryItems.className = 'gallery__item'
+	
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+	galleryImage.className = 'gallery__image'
+	galleryImage.src = element.preview
+	galleryImage.setAttribute('title', element.description)
+	galleryImage.alt = element.description
+    
+	galleryItems.append(galleryLink)
+	galleryLink.append(galleryImage)
+	items.push(galleryItems)
+})
+gallery.append(...items)
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
+
+
